5caf2defae1de03af1c5305d3d710725bbe76290
Itsgeosworld <gcrudup@my.waketech.edu>
2024-12-15 18:03:41 -0500

    new functionality to view conversation history
    The user now has the option to view conversation history by entering 1.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -17,23 +16,37 @@ prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 
-
 def conversation():
     context = ""
-    chats = []  # chats of all conversations are intialized to an empty list
-    print("Welcome to the AI Chatbot, Type 'exit' to quit")
+   # chats = []  chats of all conversations are initialized to an empty list
+    print("Welcome to the AI Chatbot, Type 'exit' to quit. Enter 1 to view conversation history.")
     while True:
         user_input = input("You: ")
         if user_input.lower() == "exit":
             break
+        if user_input.lower() == "1":
+            with open('logs.json', 'r') as file:
+                print(file.read())
+                continue
         result = chain.invoke({"context": context, "question": user_input})
         print("ChatBot: ", result)
         context += f"\nUser: {user_input}\nAI: {result}"
 
-# The user's and chatbot's responses are appended to the list 
-        chats.append({"User:": user_input, "ChatBot:": result})
-        # testing purposes which prints the user and ai's chat
-        print(context)
+        # idea:when the user enters previous session, the user's previous conversation with the model is shown
+
+        # The user's and chatbot's responses are appended to the list
+       # chats.append(context)
+
+
+        with open('logs.json','a') as file:
+            file.write(context)
+
+      #  with open("logs.json",'r') as file:
+      #      print(file.read())
+
+
+        # testing purposes which prints the user and AI's chat
+       # print(context)
 
 
 if __name__ == "__main__":
